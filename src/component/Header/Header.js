83f1bac1dd51a6ce8c9eb47e4a855529ecf5a32e
Arpitadey88 <deyarpitabanik@gmail.com>
2022-01-18 11:38:30 +0600

    added some changes

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Button, Form, FormControl, Nav, Navbar } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
+import logo from '../../img/logo.png';
 import './Header.css';
 
 const Header = () => {
@@ -9,20 +10,21 @@ const Header = () => {
             <div className="row  mx-5">
                 <Navbar>
                     <div className="col-md-2">
-                        <Navbar><i className="fas fa-guitar icon"></i>
-                            <h4 className="nav-items"><span><i>MusicSoul</i></span></h4>
+                        <Navbar><img alt="" src={logo} width="55" height="40"
+                            className="d-inline-block"
+                        /> <h4 className="nav-items logo pt-1"><span>MusicSoul</span></h4>
                         </Navbar>
                     </div>
-                    <div className=" d-flex col-md-10 ms-2">
+                    <div className="d-flex col-md-10 ms-2">
                         <div className="col-md-4">
                             <Nav className="my-2 my-lg-0">
-                                <NavLink className="list-name" to="/home"><h5 className="nav-items">Home</h5></NavLink>
+                                <NavLink className="list-name" to="/home"><h4 className="nav-items">Home</h4></NavLink>
 
-                                <NavLink className="list-name" to="/about"><h5 className="nav-items">About</h5></NavLink>
+                                <NavLink className="list-name" to="/about"><h4 className="nav-items">About</h4></NavLink>
 
-                                <NavLink className="list-name" to="/services"><h5 className="nav-items">Service</h5></NavLink>
+                                <NavLink className="list-name" to="/services"><h4 className="nav-items">Service</h4></NavLink>
 
-                                <NavLink className="list-name" to="/community"><h5 className="nav-items">Community</h5></NavLink>
+                                <NavLink className="list-name" to="/community"><h4 className="nav-items">Community</h4></NavLink>
                             </Nav>
                         </div>
                         <div className="col-md-6 search-field">
